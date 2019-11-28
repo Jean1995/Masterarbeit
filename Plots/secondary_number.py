68613b74bc7530174e344952def1b67752e8cc75
Jean-Marco Alameddine <jean-marco.alameddine@udo.edu>
2019-11-28 17:10:30 +0100

    Revisited secondary_number plot and making it nice

--- Plots/secondary_number.py
+++ Plots/secondary_number.py
@@ -103,7 +103,7 @@ class ProgressBar(object):
             self._bar[:block] = block * [self._bar_full]
         else:
             block = int(self._bar_lenght * progress)
-            self._bar[:block] = block * [self._bar_full]
+            self._bar[:block] = baxislock * [self._bar_full]
 
         text = self._text.format(
             "".join(self._bar),
@@ -118,44 +118,51 @@ class ProgressBar(object):
         sys.stdout.flush()
 
 
-def plot_hist(ax, prim, sec):
+def plot_hist(ax, prim, sec, label):
 
-    if len(prim) != 0 and len(sec) != 0:
-        hist = ax.hist2d(
-            prim,
-            sec,
-            bins=100,
-            norm=LogNorm(),
-        )
-    else:
-        hist = ax.hist2d(
-            prim,
-            sec,
-            bins=100,
-        )
+    x_space = np.logspace(  2, 14, 100)
+    y_space = np.logspace( -2, 14, 100)
 
-    ax.set_xlim(xmin=2, xmax=14)
-    ax.set_ylim(ymin=-2, ymax=14)
-    ax.grid(ls=":", lw=0.2)
+    hist = ax.hist2d(prim, sec, bins=(x_space, y_space), norm=LogNorm())
+
+    #if len(prim) != 0 and len(sec) != 0:
+    #    hist = ax.hist2d(
+    #        prim,
+    #        sec,
+    #        bins=100,
+    #        norm=LogNorm(),
+    #    )
+    #else:
+    #    hist = ax.hist2d(
+    #        prim,
+    #        sec,
+    #        bins=100,
+    #    )
+
+    #ax.set_xlim(xmin=2, xmax=14)
+    #ax.set_ylim(ymin=-2, ymax=14)
+    #ax.grid(ls=":", lw=0.2)
 
     #textstr = "count = {:g}\ntotal energy loss = {:.1e} MeV".format(
     #    sum([sum(x) for x in hist[0]]),
     #    sum([math.pow(10, x) for x in sec])
 
-    textstr = r'$\textrm{{count}} = {:g}$ \\ $E_{{\textrm{{sum}}}} = \SI{{{:.1e}}}{{\mega\electronvolt}}$'.format(
-        sum([sum(x) for x in hist[0]]),
-        sum([math.pow(10, x) for x in sec])
-
-    )
-    props = dict(facecolor='white', alpha=0.8)
-    ax.text(0.03, 0.95, textstr,
+    textstr = label
+    
+    props = dict(facecolor='white', alpha=0.8, edgecolor='none')
+    ax.text(0.1, 0.9, textstr,
             verticalalignment='top', horizontalalignment='left',
-            transform=ax.transAxes, fontsize=6, bbox=props)
+            transform=ax.transAxes, fontsize=font_size, bbox=props)
 
-    plt.colorbar(hist[3], ax=ax, pad=0.01)
-    #plt.tight_layout()
+    ax.set_xscale("log", nonposx='clip')
+    ax.set_yscale("log", nonposy='clip')
 
-    return ax
+    ax.grid(ls=":", lw=0.2)
+
+    count = sum([sum(x) for x in hist[0]])
+    esum = sum(sec)
+
+    return (ax, hist, count, esum)
 
 if __name__ == "__main__":
 
@@ -219,21 +226,21 @@ if __name__ == "__main__":
         n_secondarys.append(len(secondarys))
 
         for sec in secondarys:
-            log_sec_energy = math.log10(sec.energy)
-            log_energy = math.log10(sec.parent_particle_energy)
+            sec_energy = sec.energy
+            energy = sec.parent_particle_energy
 
             if sec.id == pyPROPOSAL.particle.Data.Epair:
-                epair_primary_energy.append(log_energy)
-                epair_secondary_energy.append(log_sec_energy)
+                epair_primary_energy.append(energy)
+                epair_secondary_energy.append(sec_energy)
             if sec.id == pyPROPOSAL.particle.Data.Brems:
-                brems_primary_energy.append(log_energy)
-                brems_secondary_energy.append(log_sec_energy)
+                brems_primary_energy.append(energy)
+                brems_secondary_energy.append(sec_energy)
             if sec.id == pyPROPOSAL.particle.Data.DeltaE:
-                ioniz_primary_energy.append(log_energy)
-                ioniz_secondary_energy.append(log_sec_energy)
+                ioniz_primary_energy.append(energy)
+                ioniz_secondary_energy.append(sec_energy)
             if sec.id == pyPROPOSAL.particle.Data.NuclInt:
-                photo_primary_energy.append(log_energy)
-                photo_secondary_energy.append(log_sec_energy)
+                photo_primary_energy.append(energy)
+                photo_secondary_energy.append(sec_energy)
 
     # =========================================================
     #   Plot
@@ -242,53 +249,33 @@ if __name__ == "__main__":
     plt.rcParams.update(params)
 
 
-    fig = plt.figure(
-        figsize=(width , 0.7*width)
+    fig, axes = plt.subplots(nrows=2, ncols=2, 
+        figsize=(width , 0.7*width), sharex=True, sharey=True
     )
-    #fig.subplots_adjust(wspace=0.1, hspace=0.3)
 
-    # =========================================================
-    #   Epair
-    # =========================================================
+    hists = []
+    counts = []
+    esums = []
+    primary_energies = [epair_primary_energy, brems_primary_energy, photo_primary_energy, ioniz_primary_energy]
+    secondary_energies = [epair_secondary_energy, brems_secondary_energy, photo_secondary_energy, ioniz_secondary_energy]
+    labels = [r'$e$ pair production', "Bremsstrahlung", "Photonuclear", "Ionization"]
 
-    ax1 = fig.add_subplot(221)
-    ax1 = plot_hist(ax1, epair_primary_energy, epair_secondary_energy)
-    ax1.set_title(r'$e$ pair production')
-    #ax1.set_ylabel(r'$ \log\left( E_{\textrm{primary}} \cdot v \,/\, \mathrm{MeV} \right)$')
+    for ax, primary_energy, secondary_energy, label in zip(axes.flat, primary_energies, secondary_energies, labels):
+    	ax, hist_tmp, count, esum = plot_hist(ax, primary_energy, secondary_energy, label)
+    	hists.append(hist_tmp[3])
+    	counts.append(count)
+    	esums.append(esums)
+    fig.tight_layout(rect=(0.02, 0.02, 1, 1)) # rect = (0,0,1,1) is the default option
 
-    # =========================================================
-    #   Brems
-    # =========================================================
-
-    ax2 = fig.add_subplot(222)
-    ax2 = plot_hist(ax2, brems_primary_energy, brems_secondary_energy)
-    ax2.set_title("Bremsstrahlung")
-
-    # =========================================================
-    #   Photonuclear
-    # =========================================================
-
-    ax3 = fig.add_subplot(223)
-    ax3 = plot_hist(ax3, photo_primary_energy, photo_secondary_energy)
-    ax3.set_title("Photonuclear")
-    #ax3.set_xlabel(r'primary particle energy log($E$/MeV)')
-    #ax1.set_ylabel(r'$ \log\left( E_{\textrm{primary}} \cdot v \,/\, \mathrm{MeV} \right)$')
-
-    # =========================================================
-    #   Ioniz
-    # =========================================================
-
-    ax4 = fig.add_subplot(224)
-    ax4 = plot_hist(ax4, ioniz_primary_energy, ioniz_secondary_energy)
-    ax4.set_title(r'Ionization')
-    #ax1.set_xlabel(r'$ \log\left( E_{\textrm{primary}} \,/\, \mathrm{MeV} \right)$')
-
+    fig.subplots_adjust(wspace=0.15, hspace=0.1, right=0.88)
+    cbar_ax = fig.add_axes([0.91, 0.15, 0.03, 0.7])
+    fig.colorbar(hists[3], cax=cbar_ax)
 
     fig.add_subplot(111, frameon=False)
     # hide tick and tick label of the big axis
     plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
-    plt.xlabel(r'$ \log\left( E_{\textrm{primary}} \,/\, \mathrm{MeV} \right)$')
-    plt.ylabel(r'$ \log\left( E_{\textrm{primary}} \cdot v \,/\, \mathrm{MeV} \right)$')
-    plt.tight_layout()
+    plt.xlabel(r'$  E_{\textrm{primary}}\,/\, \mathrm{MeV} $')
+    plt.ylabel(r'$  E_{\textrm{primary}} \cdot v \,/\, \mathrm{MeV} $', labelpad=10)
+    #fig.tight_layout()
 
     fig.savefig("build/secondary_number.pdf")
