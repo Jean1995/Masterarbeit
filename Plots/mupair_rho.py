71ac55fb0121ca57b13cf98d61093bff22bb4dd4
Jean-Marco Alameddine <jean-marco.alameddine@udo.edu>
2019-12-09 14:38:14 +0100

    Finished first version of 3.1.2. with improvments in the rho histogram plot

--- Plots/mupair_rho.py
+++ Plots/mupair_rho.py
@@ -5,6 +5,7 @@ import pyPROPOSAL.parametrization as parametrization
 from matplotlibconfig import *
 
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 
 #define defaults and parametrization
@@ -22,24 +23,38 @@ pp.RandomGenerator.get().set_seed(1234)
 param = parametrization.mupairproduction.KelnerKokoulinPetrukhinInterpolant(*param_defs_mupair)
 
 v_list = [0.1, 0.5, 0.8]
-
-rho_list = []
-
-
-energies = np.ones(1000000)*1e6
-for i, v in enumerate(v_list):
-	rho_list.append([])
-	for E in energies:
-		rho_list[i].append(param.Calculaterho(E, v, np.random.rand(), np.random.rand()))
+colors = ['tab:blue', 'tab:orange', 'tab:green']
 
 
+E_list = [1e4, 1e9]
+linestyles = ['--', '-']
 
-for i, v in enumerate(v_list):
-	plt.hist(np.abs(rho_list[i]), bins=40, histtype='step', label=r'$v = {:.2g}$'.format(v), zorder=3)
+statistics = int(1e6)
 
-plt.xlabel(r'$\rho$')
+for E_init, linestyle in zip(E_list, linestyles):
+	rho_list = []
+	energies = np.ones(statistics)*E_init
+	for i, v in enumerate(v_list):
+		rho_list.append([])
+		for E in energies:
+			rho_list[i].append(param.Calculaterho(E, v, np.random.rand(), np.random.rand()))
+
+	for i, (v, c) in enumerate(zip(v_list, colors)):
+		plt.hist(np.abs(rho_list[i]), color=c, bins=40, histtype='step', zorder=3, ls=linestyle)
+
+plt.xlabel(r'$\lvert \rho \rvert$')
 plt.ylabel(r'Frequency',)
+
+# automatic legend
 plt.legend(loc='upper left')
+custom_lines = []
+legends = []
+for v, c in zip(v_list, colors):
+	custom_lines.append(Line2D([0], [0], color=c, lw=2))
+	legends.append(r'$v = {:.2g}$'.format(v))
+
+plt.legend(custom_lines, legends, loc='upper left')
+
 plt.grid(grid_conf)
 plt.tight_layout()
 plt.savefig('build/mupair_rho.pdf',bbox_inches='tight')
