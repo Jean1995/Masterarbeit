Correct the calculation of the decay probability in figure 3.7
There has been an error in the calculation of the gamma factor. The lead to an error of around 0.1% for the lowest energies in the plot and an even smaller error for energies above. Since the plot has a double logarithmic scale, this correction does not change the plot.

Plots/dEdx_weak.py
@@ -26,7 +26,7 @@ def sigma_decay(E, rho):
     c = 2.99792458e10 #speed of light in cm / s
     tau = 2.1969811e-6 # muon life time
     m = 105.6583745 # muon mass in MeV
-    gamma = (E - m)/m
+    gamma = E/m
     beta = np.sqrt(1 - 1 / gamma**2)
     return 1 / (gamma * beta * tau * c * rho)
 